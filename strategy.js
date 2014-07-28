0c0759125bf0aebbe4ca3842cfb33f4be6630fc4
nicogiangregorio <nicogorio@gmail.com>
2014-07-29 00:01:27 +0200

    Best implementation

--- strategy.js
+++ strategy.js
@@ -1,44 +1,31 @@
-if (typeof Object.beget !== 'function') {
-  Object.beget = function(o) {
-    var F = function() {};
-    F.prototype = o;
-    return new F();
-  };
+var ClickProcessor = function(obj) {
+	this.obj = obj;
 }
 
-var leftClickProcessor = {
-	printMessage: function(elemId){
-		var element = document.getElementById(elemId);
-		element.innerHTML += '<br>Default text';
+ClickProcessor.prototype.leftClick = function() {
+	this.obj.leftClick();
+}
+
+ClickProcessor.prototype.rightClick = function() {
+	this.obj.rightClick();	
+}
+
+var TypeA = function() {
+	this.leftClick = function() {
+		console.log("TypeA left click");
 	}
-};
 
-var typeALeftClickprocessor = Object.beget(leftClickProcessor);
-typeALeftClickprocessor.printMessage = function(elemId) {
-	var element = document.getElementById(elemId);
-	element.innerHTML += '<br>Type A message';
+	this.rightClick = function() {
+		console.log("TypeA right click");
+	}
 }
 
-var typeBLeftClickprocessor = Object.beget(leftClickProcessor);
-typeBLeftClickprocessor.printMessage = function(elemId) {
-	var element = document.getElementById(elemId);
-	element.innerHTML += '<br>Type B message';
+var TypeB = function() {
+	this.leftClick = function() {
+		console.log("TypeB left click");
+	}
+
+	this.rightClick = function() {
+		console.log("TypeB right click");
+	}
 }
-
-STRATEGY_CONTEXT = function () {
-	var typeMap = [];
-
-	typeMap['TypeA'] = typeMap['TypeAA'] = typeALeftClickprocessor;
-	typeMap['TypeB'] = typeBLeftClickprocessor;
-
-	return {
-		evaluate: function(obj){
-			
-			if(!typeMap[obj.typeofobj]){
-				obj.leftClickProcessor = Object.beget(leftClickProcessor);
-			} else {
-				obj.leftClickProcessor = typeMap[obj.typeofobj];
-			}
-		}
-	};
-}();
